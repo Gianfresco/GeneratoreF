e6d65f6656f3afc97853b9c7c48ec1c18b6cbc2b
Gianfresco <gianfranco.cavallini@edu.unife.it>
2024-11-19 12:20:26 +0100

    valori posizioni aggiornati

--- Program.fs
+++ Program.fs
@@ -13,26 +13,25 @@ let main argv =
     let testoConsegna = (letturaFile argv.[0])
     let testoCodice = (letturaFile argv.[1])
 
-    let main : bool = testoCodice.Contains("main")   // usato per determinare se il codice è una funzione o un programma
     let mutable tipoDomanda = getTipoFile(argv.[1])
     if tipoDomanda.EndsWith("_") then
-        if main then
+        if testoCodice.Contains("main()") then          // usato per determinare se il codice è una funzione o un programma
             tipoDomanda <- tipoDomanda + "program"
         else
             tipoDomanda <- tipoDomanda + "function"
 
     
     let mutable pos = 0
-    pos <- pos + 231        // "cc xvv" -Romeo
+    pos <- pos + 210        // "cc xvv" -Romeo
     let insConsegna = inserisciTesto getTemplate testoConsegna pos
-    pos <- pos + 230 + testoConsegna.Length
+    pos <- pos + 251 + testoConsegna.Length
     let insTipo = inserisciTesto insConsegna tipoDomanda pos
-    pos <- pos + 586 + testoConsegna.Length
+    pos <- pos + 569 + testoConsegna.Length
     let insCodice = inserisciTesto insTipo testoCodice pos
 
     
     try
-        File.WriteAllText(Path.GetFileName(argv.[0]), insCodice)
+        File.WriteAllText((Path.GetFileNameWithoutExtension(argv.[0]) + ".xml"), insCodice)
     with
         | :? UnauthorizedAccessException ->
             printfn "Accesso negato al percorso."
